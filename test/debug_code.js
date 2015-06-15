2b6136525b9f5cbc053ffa9f92172f2a3a0a0601
Amanda <aboss@college.harvard.edu>
2015-06-15 14:53:07 -0400

    Added unhover test and working on test panel

--- test/debug_code.js
+++ test/debug_code.js
@@ -263,14 +263,6 @@ it('should be able to highlight lines when hovered', function(done) {
 		  if (!$('.preview iframe').length) return;
 		  if (!$('.preview iframe')[0].contentWindow.see) return;
 		  if (!$('.ace_gutter-cell').length) return;
-		  $(".ace_gutter-cell").mouseover(function(){
-			  hovered = true;
-			  
-		  })
-		   $(".ace_gutter-cell").mouseout(function(){
-			  unhovered = true;
-			  
-		  })
 		  window._simulate('mouseover', $(".ace_gutter-cell")[0]);
 		  
 		  if($(".debugfocus").length == 0){
@@ -279,13 +271,7 @@ it('should be able to highlight lines when hovered', function(done) {
 		  
 		  return{
 			//  poll:"polling",
-			  badtemp: unhovered,
-			  temp: hovered,
-			  debugfocus : $(".debugfocus").length,	
-			  debug: $(".debugfocus").context,
-	          gutters: $(".ace_gutter-cell").length,
-			  elem: $('.ace_gutter-cell')[0],
-		      actual: $(".ace_gutter-cell").eq(0).text()  
+			  debugfocus : $(".debugfocus").length
 		  };
       }
       catch(e) {
@@ -293,13 +279,138 @@ it('should be able to highlight lines when hovered', function(done) {
       }
     }, function(err, result) {
       assert.ifError(err);
-	//  window._simulate('mouseover', result.elem);
-	  assert.equal(false, result.badtemp);
-	  assert.equal(true, result.temp);
-	//  assert.equal(null, result.debug);
-	  assert.ok(parseInt(result.gutters) > 0);
-	  assert.equal(1, parseInt(result.actual));
 	  assert.equal(1, result.debugfocus);
+	  
+		done();
+  });
+
+});
+
+it('should be able to unhighlight lines when unhovered', function(done) {
+	
+ asyncTest(_page, one_step_timeout, null, function() {
+      // Click on the square stop button.
+      $('#run').mousedown();
+      $('#run').click();
+	 window._simulate = function simulate(type, target, options) {
+    	if ('string' == typeof(target)) {
+      	target = $(target).get(0);
+    }
+    options = options || {};
+    var pageX = pageY = clientX = clientY = dx = dy = 0;
+    var location = options.location || target;
+    if (location) {
+      if ('string' == typeof(location)) {
+        location = $(location).get(0);
+      }
+      var gbcr = location.getBoundingClientRect();
+      clientX = gbcr.left,
+      clientY = gbcr.top,
+      pageX = clientX + window.pageXOffset;
+      pageY = clientY + window.pageYOffset;
+      dx = Math.floor((gbcr.right - gbcr.left) / 2);
+      dy = Math.floor((gbcr.bottom - gbcr.top) / 2);
+    }
+    if ('dx' in options) dx = options.dx;
+    if ('dy' in options) dy = options.dy;
+    pageX = (options.pageX == null ? pageX : options.pageX) + dx;
+    pageY = (options.pageY == null ? pageY : options.pageY) + dy;
+    clientX = pageX - window.pageXOffset;
+    clientY = pageY - window.pageYOffset;
+    var opts = {
+        bubbles: options.bubbles || true,
+        cancelable: options.cancelable || true,
+        view: options.view || target.ownerDocument.defaultView,
+        detail: options.detail || 1,
+        pageX: pageX,
+        pageY: pageY,
+        clientX: clientX,
+        clientY: clientY,
+        screenX: clientX + window.screenLeft,
+        screenY: clientY + window.screenTop,
+        ctrlKey: options.ctrlKey || false,
+        altKey: options.altKey || false,
+        shiftKey: options.shiftKey || false,
+        metaKey: options.metaKey || false,
+        button: options.button || 0,
+        which: options.which || 1,
+        relatedTarget: options.relatedTarget || null,
+    }
+    var evt;
+    try {
+      // Modern API supported by IE9+
+      evt = new MouseEvent(type, opts);
+	  
+    } catch (e) {
+      // Old API still required by PhantomJS.
+      evt = target.ownerDocument.createEvent('MouseEvents');
+      evt.initMouseEvent(type, opts.bubbles, opts.cancelable, opts.view,
+        opts.detail, opts.screenX, opts.screenY, opts.clientX, opts.clientY,
+        opts.ctrlKey, opts.altKey, opts.shiftKey, opts.metaKey, opts.button,
+        opts.relatedTarget);
+    }
+    target.dispatchEvent(evt);
+	}
+    }, function() {
+      try {
+		  var hovered = false;
+		  var unhovered = false;
+		  if (!$('.preview iframe').length) return;
+		  if (!$('.preview iframe')[0].contentWindow.see) return;
+		  if (!$('.ace_gutter-cell').length) return;
+		  window._simulate('mouseover', $(".ace_gutter-cell")[0]);
+		  window._simulate('mouseout', $(".ace_gutter-cell")[0]);
+		  
+	  if($(".debugfocus").length != 0){
+			  return;
+		  }
+		  
+		  return{
+			  debugfocus : $(".debugfocus").length,	
+		  };
+      }
+      catch(e) {
+        return {poll: true, error: e};
+      }
+    }, function(err, result) {
+      assert.ifError(err);
+	  assert.equal(0, result.debugfocus);
+	  
+		done();
+  });
+
+  });
+  
+  it('should not trace commands in the test panel', function(done) {
+	
+ asyncTest(_page, one_step_timeout, null, function() {
+      // Click on the square stop button.
+      $('#run').mousedown();
+      $('#run').click();
+	  
+    }, function() {
+      try {
+		  
+		  if (!$('.preview iframe').length) return;
+		  if (!$('.preview iframe')[0].contentWindow.see) return;
+		  if($('.debugtrace').length != 0){
+			  return;
+		  }
+		  var seval = $('.preview iframe')[0].contentWindow.see.eval;
+		  seval('fd 100');
+		  return {
+		  poll: "polling",
+          debugtracecount: $('.debugtrace').length,
+          debugtracetop: $('.debugtrace').css('top')
+	  };
+		  
+      }
+      catch(e) {
+        return {poll: true, error: e};
+      }
+    }, function(err, result) {
+      assert.ifError(err);
+	  assert.equal(0, result.debugtracecount);
 	  
 		done();
   });
